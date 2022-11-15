aa8490dbdc1d13d130172b454a52ac6566f5ad66
likecodingloveproblems <mohammadali.mehdizadeh@outlook.com>
2022-11-15 22:08:41 +0330

    this is better

--- integer-to-roman.go
+++ integer-to-roman.go
@@ -9,37 +9,33 @@ func main() {
 
 func intToRoman(num int) string {
 	var result string
-	decimalKeys := []int{1000, 500, 100, 50, 10, 5, 1}
+	var roman string
+	decimals := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 	decimalToRoman := map[int]string{
 		1000: "M",
+		900:  "CM",
 		500:  "D",
+		400:  "CD",
 		100:  "C",
+		90:   "XC",
 		50:   "L",
+		40:   "XL",
 		10:   "X",
+		9:    "IX",
 		5:    "V",
+		4:    "IV",
 		1:    "I",
 	}
-	combKeys := []int{900, 400, 90, 40, 9, 4}
-	combinationMap := map[int]string{
-		900: "CM",
-		400: "CD",
-		90:  "XC",
-		40:  "XL",
-		9:   "IX",
-		4:   "IV",
-	}
 
-	for i, decimal := range decimalKeys {
-		if num/decimal > 0 {
-			for i := 0; i < num/decimal; i++ {
-				result = result + decimalToRoman[decimal]
+	for _, decimal := range decimals {
+		roman = decimalToRoman[decimal]
+		if count := num / decimal; count > 0 {
+			for i := 0; i < count; i++ {
+				result = result + roman
 			}
-			num = num - (num/decimal)*decimal
+			num = num - count*decimal
 		}
-		if i < len(combKeys) && num/combKeys[i] == 1 {
-			result = result + combinationMap[combKeys[i]]
-			num = num - (num/combKeys[i])*combKeys[i]
-		}
+
 	}
 	return result
 }
